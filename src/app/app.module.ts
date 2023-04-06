d6e246be326fc5d9891160b6b1f44cf5af2ce7a3
denisamihut <denisa.m_19@yahoo.com>
2023-04-06 12:18:00 +0300

    created forecast component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import {AppComponent} from './app.component';
 import {TodayComponent} from './today/today.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodayComponent
+    TodayComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,

1d51e83ec898b2d307f631f0f53ba7383f2171f4
denisamihut <denisa.m_19@yahoo.com>
2023-04-06 10:38:01 +0300

    displayed API data in todays weather card

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "../weather.service";
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.css']
 })
-export class TodayComponent {
+export class TodayComponent implements OnInit {
+  myWeather: any;
+  city: string = 'London';
+  iconURL: string = '';
+  summary: string = '';
+  temperature: number = 0;
+  feelsLikeTemp: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
 
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.weatherService.getWeather(this.city).subscribe({
+
+      next: (res) => {
+        console.log(res)
+        this.myWeather = res;
+        console.log(this.myWeather)
+        this.iconURL = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png';
+        this.summary = this.myWeather.weather[0].main;
+        this.temperature = this.myWeather.main.temp;
+        this.feelsLikeTemp = this.myWeather.main.feels_like;
+        this.humidity = this.myWeather.main.humidity;
+        this.pressure = this.myWeather.main.pressure;
+      },
+
+      error: (error) => console.log(error.message),
+
+      complete: () => console.info('API call completed')
+    })
+  }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private http: HttpClient) {
   }
 
-  getWeather() {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=5aecdebfad2b5e742ec7ed4f5ccb40e0&units=metric')
+  getWeather(city: string) {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5aecdebfad2b5e742ec7ed4f5ccb40e0&units=metric')
   }
 }

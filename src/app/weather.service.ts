1fb6b0ab22fe5569a7a21bb63e5fb800e24a53d5
denisamihut <denisa.m_19@yahoo.com>
2023-04-11 17:50:58 +0300

    changed display in html files

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -5,11 +5,16 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class WeatherService {
+  myApiKey: string = '5aecdebfad2b5e742ec7ed4f5ccb40e0';
 
   constructor(private http: HttpClient) {
   }
 
   getWeather(city: string) {
-    return this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=5aecdebfad2b5e742ec7ed4f5ccb40e0&units=metric')
+    return this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + this.myApiKey + '&units=metric')
+  }
+
+  getForecast() {
+    return this.http.get('https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=5aecdebfad2b5e742ec7ed4f5ccb40e0')
   }
 }
